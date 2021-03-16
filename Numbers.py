d699153cfe0a1a98f775a5776a7d7857572e88cb
Ken <mulilamwendwa@gmail.com>
2021-03-16 14:48:06 +0300

    Add files via upload
    working with numbers

---
+++ Numbers.py
@@ -0,0 +1,23 @@
+#working with numbers
+#other functions need to imported using
+from math import *
+print(2)
+#mathematical equations with order operations
+print(5*(8+6))
+#modulus
+print(10%3)
+#converting numbers into strings
+age=40
+print("my age is "+str(age))
+#returning absolute value where abs stands for absolute
+age=-40
+print(abs(age))
+#power
+print(pow(4,6))
+#returning the maximum value
+print(max(4,6))
+print(min(4,6))
+#round a number to a whole number
+print(round(4.6))
+print(sqrt(4))
+
