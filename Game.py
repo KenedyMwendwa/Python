df80261726b01b475f63beeef17a1086947ab327
Ken <mulilamwendwa@gmail.com>
2021-03-16 14:53:46 +0300

    Add files via upload
    kids game

---
+++ Game.py
@@ -0,0 +1,9 @@
+#my first kidsgame
+county= input("Enter the name of county govt that manages a hub in Kenya: ")
+name= input ("Enter the name of the govenor in that county: ")
+profession= input("Enter the profession of the governor in that county: ")
+print(county + " county is the only county that has an hub managed under county government. ")
+print("It is led by his excellency professor " + name)
+print("He is a " + profession)
+
+#the last min(1.03.10)
