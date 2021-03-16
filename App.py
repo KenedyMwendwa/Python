de1860af66ade6f22e00936ed5094d22ed54dd39
Ken <mulilamwendwa@gmail.com>
2021-03-16 14:46:34 +0300

    Add files via upload
    first code

---
+++ App.py
@@ -0,0 +1,40 @@
+print("I have started")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+#data types
+#string
+name="Kenedy Mulila"
+
+#integer
+Age= 24
+
+#boolean
+isAge= True
+
+#working with strings
+#printing strings in different lines
+print("Sama\n ni\n munaa")
+
+#string concatination
+jina= "sama"
+print(jina+" ni munaa")
+#function for converting strings to upper and lower cases purely
+print(jina.lower())
+print(jina.upper())
+#checking whether upper or lowercase
+print(jina.islower())
+print(jina.isupper())
+#converting and then checking
+print(jina.upper().islower())
+print(jina.upper().isupper())
+#checking length in strings
+print(len(jina))
+#using indexs to retreive a character depending on number
+print(jina[2])
+#replace things in strings
+print(jina.replace("sama","ken"))
+
+
