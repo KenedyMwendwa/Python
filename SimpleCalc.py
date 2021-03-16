2dddb7fa21dbff4b035cdeab8b6698bd3f4231b1
Ken <mulilamwendwa@gmail.com>
2021-03-16 14:48:56 +0300

    Add files via upload
    Simple calculator in python

---
+++ SimpleCalc.py
@@ -0,0 +1,14 @@
+#enter two numbers
+num1=input("Enter number 1: ")
+num2=input("Enter number 2: ")
+#the inputs are automatically strings hence they need to be converted to integers
+addition= int(num1) + int(num2)
+substraction= int(num1) - int(num2)
+multiplication = int(num1) * int(num2)
+division= int(num1)/int(num2)
+modulus= int(num1)%int(num2)
+print(addition)
+print(substraction)
+print(multiplication)
+print(division)
+print(modulus)
