ddc2d670a8d29e3bce114b14af669f77fbd3ead9
Ken <mulilamwendwa@gmail.com>
2021-03-19 11:51:00 +0300

    Add files via upload
    Basic application of while loop in Python

---
+++ While loop.py
@@ -0,0 +1,9 @@
+#The while loop is based on conditions passed
+#it keeps iterating till the condition is passed
+#example
+i = 1
+while i <= 6:
+    print(i)
+    i += 1
+
+print("Done with looping")
