b904ba242a0077b1a770acc54d3c71ce1fcf02cf
Ken <mulilamwendwa@gmail.com>
2021-03-19 11:53:07 +0300

    Add files via upload
    Makueni county Kids BootCamp game

---
+++ Mkaueni county Bootgame.py
@@ -0,0 +1,22 @@
+#the game shall require the kids to keep guessing the name of their county
+#we can also ensure that the kids will only win after winning a couple number of times
+secret_word = "MAKUENI"
+guess = ""
+#set the number of guessing times
+guessCount = 0
+#set the limit
+guessLimit = 3
+#throw on quess limit remaining
+outOfGuesses = False
+while guess != secret_word and not(outOfGuesses):
+    if guessCount < guessLimit:
+        guess = input("Enter guess capital letters: ")
+#Increment the guesses
+        guessCount += 1
+    else:
+        outOfGuesses = True
+if outOfGuesses:
+    print("Out of Guesses, YOU LOSE")
+else:
+    
+    print("You win")
